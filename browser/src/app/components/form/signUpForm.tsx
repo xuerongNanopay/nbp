e27d0ec908079bef302924e0ca83e4d04643ad82
xuerongNanopay <xuerong@nanopay.net>
2023-09-10 23:40:58 -0400

    add Yup validation

--- browser/src/app/components/form/signUpForm.tsx
+++ browser/src/app/components/form/signUpForm.tsx
@@ -1,24 +1,48 @@
 'use client'
 
-import { Formik } from "formik"
+import { useFormik } from "formik"
+import * as Yup from 'yup';
+import {
+  Input
+} from "@nextui-org/react";
 
 interface ISignUp {
   email: string,
   username: string,
-  password: string
+  password: string,
+  rePassword: string,
 }
 
+const getCharacterValidationError = (str: string) => {
+  return `Your password must have at least 1 ${str} character`;
+};
+
 export default function SignUpForm() {
-  const initialValue: ISignUp = {email: '', username: '', password: ''}
+  const initialValues: ISignUp = {email: '', username: '', password: '', rePassword: ''}
+
   const signUp = (e: ISignUp) => {}
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: {
+      email: Yup.string().email('Invalid email address').required('Required'),
+      username: Yup.string().min(8, 'Must between 8 to 24 characters').max(24, 'Must between 8 to 24 characters').required('Required'),
+      password: Yup.string()
+                  .min(8, "Password must have at least 8 characters")
+                  .matches(/[0-9]/, getCharacterValidationError("digit"))
+                  .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+                  .matches(/[A-Z]/, getCharacterValidationError("uppercase")),
+      rePassword: Yup.string().required("Please re-type your password").oneOf([Yup.ref("password")], "Passwords does not match")
+    },
+    onSubmit: values => { console.log(values) }
+  })
+
   return (
     <>
-      <Formik
-        initialValues={initialValue}
-        onSubmit={signUp}
-      >
+      <h4 className="text-2xl font-bold">Create An Account</h4>
+      <form onSubmit={formik.handleSubmit}>
 
-      </Formik>
+      </form>
     </>
   )
 }
