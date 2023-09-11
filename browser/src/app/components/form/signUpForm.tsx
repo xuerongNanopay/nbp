4b991f17ba8e2f9d0b0254f5e6f39d2eed8da88c
xuerongNanopay <xuerong@nanopay.net>
2023-09-10 22:58:22 -0400

    install formik

---
+++ browser/src/app/components/form/signUpForm.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function SignUpForm() {
+  return (
+    <h1>SignUp Form</h1>
+  )
+}
--- browser/src/app/page.tsx
+++ browser/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import SignUpForm from "./components/form/signUpForm"
 
 export default function Home() {
   return (
     <main className="bg-slate-400 min-h-screen grid place-content-center">
-      foam
+      <SignUpForm></SignUpForm>
     </main>
   )
 }
