ad0b3ea04e3ab0aed021e112e7effb2df9a87b7e
xuerongNanopay <xuerong@nanopay.net>
2023-09-10 22:40:56 -0400

    NBP project

---
+++ browser/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="bg-slate-400 min-h-screen grid place-content-center">
+      foam
+    </main>
+  )
+}
