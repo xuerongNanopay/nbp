37d361f283521029adcd0a663381db244ab53964
xuerongNanopay <xuerong@nanopay.net>
2023-09-11 19:47:50 -0400

    customize view

--- browser/src/app/page.tsx
+++ browser/src/app/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "./components/form/signUpForm"
 
 export default function Home() {
   return (
-    <main className="bg-slate-400 min-h-screen grid place-content-center">
+    <main className="bg-slate-100 min-h-screen grid place-content-center">
       <section className="w-96">
         <SignUpForm></SignUpForm>
       </section>
