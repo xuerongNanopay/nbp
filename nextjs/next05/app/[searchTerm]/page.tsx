d3cb70996aeaba7c534fd384ef2c198e489baf4d
xuerongNanopay <xuerong@nanopay.net>
2023-09-10 21:32:11 -0400

    fetch data from wiki

---
+++ nextjs/next05/app/[searchTerm]/page.tsx
@@ -0,0 +1,26 @@
+import getWikiResults from "../lib/getWikiResults"
+
+type Props = {
+  params: {
+    searchTerm: string
+  }
+}
+
+export default async function SearchResults({params: {searchTerm}}: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+  const data = await wikiData
+  const results: Result[] | undefined = data?.query?.pages
+
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+      {results ?
+        Object.values(results).map(result => {
+          return <p>{JSON.stringify(result)}</p>
+        })
+        : <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
+      }
+    </main>
+  )
+
+  return content
+}
