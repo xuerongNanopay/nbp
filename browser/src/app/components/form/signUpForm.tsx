a81ae0ac4c36e70197c46ad54c1d77d2c025b460
xuerongNanopay <xuerong@nanopay.net>
2023-09-11 23:11:39 -0400

    add icon for password field

--- browser/src/app/components/form/signUpForm.tsx
+++ browser/src/app/components/form/signUpForm.tsx
@@ -21,10 +21,9 @@ export default function SignUpForm() {
   const initialValues: ISignUp = {email: '', username: '', password: '', rePassword: ''}
 
   const signUp = (e: ISignUp) => {}
-
   const formik = useFormik({
     initialValues,
-    validationSchema: {
+    validationSchema: Yup.object({
       email: Yup.string().email('Invalid email address').required('Required'),
       username: Yup.string().min(8, 'Must between 8 to 24 characters').max(24, 'Must between 8 to 24 characters').required('Required'),
       password: Yup.string()
@@ -33,15 +32,31 @@ export default function SignUpForm() {
                   .matches(/[a-z]/, getCharacterValidationError("lowercase"))
                   .matches(/[A-Z]/, getCharacterValidationError("uppercase")),
       rePassword: Yup.string().required("Please re-type your password").oneOf([Yup.ref("password")], "Passwords does not match")
-    },
+      
+    }),
     onSubmit: values => { console.log(values) }
   })
 
   return (
     <div className="max-w-sm">
       <h4 className="text-2xl font-bold mb-4">Create An Account</h4>
-      <form onSubmit={formik.handleSubmit}>
-        <Input type="email" variant="bordered" color="primary" label="Email" />
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-2">
+        <Input
+          id="email"
+          type="email" 
+          variant="bordered" 
+          label="Email"
+          {...formik.getFieldProps('email')}
+          errorMessage={formik.touched.email && formik.errors.email}
+        />
+        <Input
+          id="username"
+          type="text" 
+          variant="bordered" 
+          label="Username"
+          {...formik.getFieldProps('username')}
+          errorMessage={formik.touched.username && formik.errors.username}
+        />
       </form>
     </div>
   )
