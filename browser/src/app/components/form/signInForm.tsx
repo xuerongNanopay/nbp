7ad9d6473cf590a61484a5ec2ff56452295a1f0d
xuerongNanopay <xuerong@nanopay.net>
2023-09-12 21:41:28 -0400

    add signIn flow

---
+++ browser/src/app/components/form/signInForm.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { useState } from "react"
+import { useFormik } from "formik"
+import * as Yup from 'yup';
+import {
+  Input,
+  Button
+} from "@nextui-org/react";
+
+import { EyeSlashFilledIcon } from "@/app/icons/EyeSlashFilledIcon"
+import { EyeFilledIcon } from "@/app/icons/EyeFilledIcon"
+
+export default function SignInForm() {
+  const [ isPasswordVisible, setIsPasswordVisible ] = useState(false)
+  const initialValues: ISignIn = {email: '', password: ''}
+
+  const signIn = (e: ISignIn) => { console.log(e) }
+  const formik = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string().required('Required')
+    }),
+    onSubmit: signIn
+  })
+
+  return (
+    <div className="max-w-sm">
+      <h4 className="text-2xl font-bold mb-6 text-center">Welcome Back</h4>
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
+        <Input
+          id="email"
+          type="email" 
+          variant="bordered" 
+          label="Email"
+          color="primary"
+          {...formik.getFieldProps('email')}
+          errorMessage={formik.touched.email && formik.errors.email}
+        />
+        <Input
+          id="password"
+          label="Password"
+          variant="bordered"
+          color="primary"
+          endContent={
+            <button className="focus:outline-none" type="button" onClick={() =>setIsPasswordVisible(pre => !pre)}>
+              {isPasswordVisible ? (
+                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </button>
+          }
+          type={isPasswordVisible ? "text" : "password"}
+          {...formik.getFieldProps('password')}
+          errorMessage={formik.touched.password && formik.errors.password}
+        />
+        <Button 
+          type="submit"
+          color="primary"
+          className="mt-4"
+          isDisabled={!(formik.isValid && formik.dirty)}
+        >
+          Sign In
+        </Button>
+      </form>
+    </div>
+  )
+}
--- browser/src/app/page.tsx
+++ browser/src/app/page.tsx
@@ -1,10 +1,12 @@
 import SignUpForm from "./components/form/signUpForm"
+import SignInForm from "./components/form/signInForm"
 
 export default function Home() {
   return (
     <main className="nbp min-h-screen grid place-content-center">
       <section className="w-96">
-        <SignUpForm></SignUpForm>
+        {/* <SignUpForm></SignUpForm> */}
+        <SignInForm></SignInForm>
       </section>
     </main>
   )
