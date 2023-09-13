f4a01cc2a2889e431e5f29090d6140772d7f49ab
xuerongNanopay <xuerong@nanopay.net>
2023-09-12 22:41:50 -0400

    EmailVerifyForm unfinished

---
+++ browser/src/app/components/form/emailVerifyForm.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState } from "react"
+import { useFormik } from "formik"
+import * as Yup from 'yup';
+import {
+  Input,
+  Button
+} from "@nextui-org/react";
+
+import React from 'react'
+
+export default function EmailVerifyForm() {
+  const initialValues: IEmailVerify = {code: ''}
+
+  const emailVerifyFormHandle = (e: IEmailVerify) => { console.log(e) }
+  const resetCodeHandler = () => { alert("TODO: send user new code") }
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+    onSubmit: emailVerifyFormHandle
+  })
+
+  return (
+    <div>EmailVerifyForm</div>
+  )
+}
