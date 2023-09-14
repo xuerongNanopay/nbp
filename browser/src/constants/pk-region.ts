18818afb2a33510b9e23f965008d2b0ee7408c02
xuerongNanopay <xuerong@nanopay.net>
2023-09-13 21:55:10 -0400

    add regions for PK

---
+++ browser/src/constants/pk-region.ts
@@ -0,0 +1,33 @@
+const PKRegion = [
+  {
+    "id": "PK-JK",
+    "isoCode": "JK",
+    "name": "Azad Jammu and Kashmir"
+  },
+  {
+    "id": "PK-BA",
+    "isoCode": "BA",
+    "name": "Balochistan",
+  },
+  {
+    "id": "PK-GB",
+    "isoCode": "GB",
+    "name": "Gilgit-Baltistan"
+  },
+  {
+    "id": "PK-KP",
+    "isoCode": "KP",
+    "name": "Khyber Pakhtunkhwa"
+  },
+  {
+    "id": "PK-PB",
+    "isoCode": "PB",
+    "name": "Punjab"
+  },
+  {
+    "id": "PK-SD",
+    "isoCode": "SD",
+    "name": "Sindh"
+  }
+]
+export default PKRegion
