e745eee3c39b6901bf6f7d8bd6f14a309946a0c8
xuerongNanopay <xuerong@nanopay.net>
2023-09-14 21:46:08 -0400

    setup project

--- nextjs/next06/app/page.tsx
+++ nextjs/next06/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="px-6 mx-auto">
+      <h1 className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome &nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Dave</span>
+        </span>
+      </h1>
     </main>
   )
 }
