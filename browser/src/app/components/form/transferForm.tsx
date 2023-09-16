70b557ddceec085c46b156a1aa1d9b4cd279db56
xuerongNanopay <xuerong@nanopay.net>
2023-09-16 19:05:33 -0400

    TODO: rethink flow

--- browser/src/app/components/form/transferForm.tsx
+++ browser/src/app/components/form/transferForm.tsx
@@ -11,6 +11,26 @@ import {
 } from "@nextui-org/react";
 
 export default function TransferForm() {
+  const initialValues: ITransferQuote = {
+    sourceAcount: 0,
+    destinationAccount: 0,
+    sourceCurreccy: 'CAD',
+    destinationCurrency: 'PRK',
+    sourceAmount: 0,
+    destinationAmount: 0
+  }
+
+  const transferQuoteHandler = (e: ITransferQuote) => {
+    console.log(e)
+  }
+
+  const formick = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+
+    }),
+    onSubmit: transferQuoteHandler
+  })
   return (
     <div>TransferForm</div>
   )
