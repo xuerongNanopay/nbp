173caf4603d742b945622bb0c13101558d12cbd0
xuerongNanopay <xuerong@nanopay.net>
2023-09-16 20:11:55 -0400

    add Post

--- nextjs/next06/app/posts/[postId]/page.tsx
+++ nextjs/next06/app/posts/[postId]/page.tsx
@@ -1,5 +1,8 @@
 import { getSortedPostsData } from "@/lib/posts"
 import { notFound } from 'next/navigation'
+import { getPostData } from "@/lib/posts"
+import getFormattedDate from "@/lib/getFormattedDate"
+import Link from 'next/link'
 
 export function generateMetadata({ params: { postId} }: { params: { postId: string }}) {
   const posts = getSortedPostsData()
@@ -19,12 +22,22 @@ export default async function Post({ params: { postId} }: { params: { postId: st
   const posts = getSortedPostsData()
   const post = posts.find((post) => post.id == postId)
 
+  if ( !post ) return notFound()
+
+  const { title, date, contentHtml } = await getPostData(postId)
+
+  const pubDate = getFormattedDate(date)
+
   return (
-    <>
-      {
-        !post ? notFound() :
-        <p>{post.id}</p>
-      }
-    </>
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{__html: contentHtml}}/>
+        <p>
+          <Link href="/">Back to home</Link>
+        </p>
+      </article>
+    </main>
   )
 }
