5aea875e105ec8baf88fd47043c8e96873eb61b8
xuerongNanopay <xuerong@nanopay.net>
2023-09-17 01:43:00 -0400

    transaction submit finish

--- browser/src/app/components/form/submitTransactionForm.tsx
+++ browser/src/app/components/form/submitTransactionForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 import {
   Button
@@ -14,6 +14,7 @@ export default function SubmitTransactionForm({ quoteId }: prop) {
   //TODO: using quoteId to fetch transaction detail.
 
   const [transactionQuoteResult, setTransactionQuoteResult] = useState<ITransferQuoteResult|null>(null)
+  const [isSubmitting, setIsSubmitting ] = useState(false)
 
   useEffect(() => {
     async function fetchQuoteResult(quoteId: string) {
@@ -44,24 +45,52 @@ export default function SubmitTransactionForm({ quoteId }: prop) {
     }
     fetchQuoteResult(quoteId)
   }, [])
-  const handleTransactionTraction = (quoteId: string) => {
+  const handleTransactionTraction = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsSubmitting(true)
     console.log(quoteId)
   }
   return (
     <div className="w-full max-w-xl">
       <h4 className="text-2xl font-bold mb-6 text-center">Confirm Transaction</h4>
+      <p></p>
       <form 
-        onSubmit={() => handleTransactionTraction(quoteId)}
+        onSubmit={handleTransactionTraction}
         className="flex flex-col gap-4"
       >
+        {
+          transactionQuoteResult &&
+          <section className="flex flex-col">
+            <div className="flex justify-between">
+              <h6>Amount To Be Converted</h6>
+              <p>${`${transactionQuoteResult.sourceAmount} ${transactionQuoteResult.sourceCurrency}`}</p>
+            </div>
+            <div className="flex justify-between">
+              <h6>Exchange Rate</h6>
+              <p>{`$1.00 ${transactionQuoteResult.sourceCurrency} = $${transactionQuoteResult.exchangeRate.toFixed(2)} ${transactionQuoteResult?.destinationCurrency}`}</p>
+            </div>
+            <div className="flex justify-between">
+              <h6>Recipient Receives</h6>
+              <p>${`${transactionQuoteResult.destinationAmount} ${transactionQuoteResult.destinationCurrency}`}</p>
+            </div>
+            <div className="flex justify-between">
+              <h6>Transaction Fee</h6>
+              <p>${`${transactionQuoteResult.transactionFee} ${transactionQuoteResult.sourceCurrency}`}</p>
+            </div>
+            <div className="flex justify-between">
+              <h6>Amount To Be Debited</h6>
+              <p>${`${transactionQuoteResult.totalDebitAmount} ${transactionQuoteResult.sourceCurrency}`}</p>
+            </div>
+          </section>
+        }
         <Button 
           type="submit"
           color="primary"
           className="mt-6"
           size="md"
-          isDisabled={!transactionQuoteResult}
+          isLoading={!transactionQuoteResult || isSubmitting}
         >
-          SubmitTrasaction
+          Submit Trasaction
         </Button>
       </form>
     </div>
