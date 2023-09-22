fcf2a0bc41dab98eea4ce78033777249cace2fcc
xuerongNanopay <xuerong@nanopay.net>
2023-09-21 20:41:11 -0400

    add router

---
+++ nextjs/next07/src/app/api/feedback/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server"
+
+type Feedback = {
+  name?: string,
+  email?: string,
+  message?: string,
+}
+
+export async function POST(request: Request) {
+  const data: Feedback = await request.json()
+
+  console.log(data)
+
+  const { name, email, message } = data
+  
+  return NextResponse.json({name, email, message})
+}
