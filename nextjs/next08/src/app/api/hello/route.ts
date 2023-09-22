e77685ecfcb1dc896424310ed57ab1c9df1fd8e7
xuerongNanopay <xuerong@nanopay.net>
2023-09-22 15:31:33 -0400

    add limiter

--- nextjs/next08/src/app/api/hello/route.ts
+++ nextjs/next08/src/app/api/hello/route.ts
@@ -1,3 +1,16 @@
+import { NextRequest, NextResponse } from "next/server"
+import { limiter } from "../config/limiter"
+
 export async function GET(request: Request) {
+
+  const remaining = await limiter.removeTokens(1)
+  console.log(remaining)
+  if ( remaining < 0 ) return new NextResponse(null, {
+    status: 429,
+    statusText: "Too Many Requests",
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
   return new Response('Hello, Next.js!')
 }
