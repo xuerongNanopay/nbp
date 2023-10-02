066e440e80ae18dedc771acb01cdb98afa114fa1
xuerongNanopay <xuerong@nanopay.net>
2023-10-01 22:13:19 -0400

    simple component to display upload files

--- nextjs/next_upload/src/app/upload-button/page.tsx
+++ nextjs/next_upload/src/app/upload-button/page.tsx
@@ -3,23 +3,53 @@
 // You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
 import "@uploadthing/react/styles.css";
 
-import { UploadButton } from "@/utils/uploadthings";
+import { UploadButton } from "@/utils/uploadthings"
+import type UploadFileResponse from '@uploadthing/react'
+import { useState } from 'react'
+import Link from 'next/link'
 
 export default function UploadButtonPage() {
+  const [images, setImages] = useState< {fileKey: string, fileUrl: string}[]>([])
+
+  const title = images.length ? (
+    <>
+      <p>Upload Complete!</p>
+      <p className="mt-2">{images.length}</p>
+    </>
+  ) : null
+
+  const imgList = (
+    <>
+      {title}
+      <ul>
+        {images.map(image => (
+          <li key={image.fileUrl} className="mt-2">
+            <Link href={image.fileUrl} target="_blank">
+              {image.fileUrl}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <UploadButton
-      endpoint="imageUploader"
-      onClientUploadComplete={(res) => {
-        // Do something with the response
-        console.log("Files: ", res);
-        alert("Upload Completed");
-      }}
-      onUploadError={(error: Error) => {
-        // Do something with the error.
-        alert(`ERROR! ${error.message}`);
-      }}
-    />
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          if ( res ) {
+            setImages(res)
+            const json = JSON.stringify(res)
+            // Do something with the response
+            console.log("Files: ", json);
+          }
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+      {imgList}
     </main>
     );
   }
--- nextjs/next_upload/src/app/upload-dnd/page.tsx
+++ nextjs/next_upload/src/app/upload-dnd/page.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+"use client";
 
-export default function UploadDnD() {
-  return (
-    <div>UploadDnD</div>
+// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+import "@uploadthing/react/styles.css";
+
+import { UploadDropzone } from "@/utils/uploadthings"
+import { useState } from 'react'
+import Link from 'next/link'
+
+export default function UploadDNDPage() {
+  const [images, setImages] = useState< {fileKey: string, fileUrl: string}[]>([])
+
+  const title = images.length ? (
+    <>
+      <p>Upload Complete!</p>
+      <p className="mt-2">{images.length}</p>
+    </>
+  ) : null
+
+  const imgList = (
+    <>
+      {title}
+      <ul>
+        {images.map(image => (
+          <li key={image.fileUrl} className="mt-2">
+            <Link href={image.fileUrl} target="_blank">
+              {image.fileUrl}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
-}
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <UploadDropzone
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          if ( res ) {
+            setImages(res)
+            const json = JSON.stringify(res)
+            // Do something with the response
+            console.log("Files: ", json);
+          }
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+      {imgList}
+    </main>
+    );
+  }
