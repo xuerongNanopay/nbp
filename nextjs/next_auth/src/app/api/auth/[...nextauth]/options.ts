8e767cc8aa6295159094560266547cfd35b7848e
xuerongNanopay <xuerong@nanopay.net>
2023-11-12 22:54:29 -0500

    refactor page

--- nextjs/next_auth/src/app/api/auth/[...nextauth]/options.ts
+++ nextjs/next_auth/src/app/api/auth/[...nextauth]/options.ts
@@ -8,7 +8,32 @@ export const options: NextAuthOptions = {
 
   // },
   providers: [
-
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "xwu" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        //TODO: load from database.
+        // const res = await fetch("/your/endpoint", {
+        //   method: 'POST',
+        //   body: JSON.stringify(credentials),
+        //   headers: { "Content-Type": "application/json" }
+        // })
+        // const user = await res.json()
+  
+        // // If no error and we have user data, return it
+        // if (res.ok && user) {
+        //   return user
+        // }
+        // // Return null if user data could not be retrieved
+        console.log(credentials)
+        console.log(req.headers?.method)
+        return { username: 'xuerong', email: 'xuerong@nanopay.net', id: '1'}
+        // return null
+      }
+    })
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
