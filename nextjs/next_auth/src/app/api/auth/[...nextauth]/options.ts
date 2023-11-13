4e84152a57c877e132881ea33cba06a199bda543
xuerongNanopay <xuerong@nanopay.net>
2023-11-12 22:30:15 -0500

    add signIn and jwt callback

--- nextjs/next_auth/src/app/api/auth/[...nextauth]/options.ts
+++ nextjs/next_auth/src/app/api/auth/[...nextauth]/options.ts
@@ -4,9 +4,27 @@ import CredentialsProvider from "next-auth/providers/credentials"
 // Generate a Random Secret.
 // openssl rand -base64 32
 export const options: NextAuthOptions = {
+  // session: {
+
+  // },
   providers: [
 
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("clasbacks.signIn: ", { user, account, profile, email, credentials })
+      return true
+    },
+    async jwt({ token, user, account, profile }) {
+      console.log("clasbacks.signIn: ", { user, account, profile });
+      return token
+    }
+  },
+  events: {
+    async signIn(message) {
+      console.log('events.signIn', message)
+    }
+  }
   // Customize Auth page. refer to Next 
   // pages: {
   //   signIn: '/signin'
