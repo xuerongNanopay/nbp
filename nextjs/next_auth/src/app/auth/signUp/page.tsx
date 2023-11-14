d3a25e7b0aba7595099b1478c3daf6b186825d41
xuerongNanopay <xuerong@nanopay.net>
2023-11-13 23:24:36 -0500

    add trick to signUP

--- nextjs/next_auth/src/app/auth/signUp/page.tsx
+++ nextjs/next_auth/src/app/auth/signUp/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { signIn } from "next-auth/react"
 
 import { useRouter } from 'next/navigation'
-
+//Trick: SignIn again.
 export default function SignIn() {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
