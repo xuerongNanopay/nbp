8073f5973aea0ac091dd71f134ac71f3c9cdb01f
xuerongNanopay <xuerong@nanopay.net>
2023-11-18 22:59:02 -0500

    finish layout for main menu

--- browser/src/app/(main)/layout.tsx
+++ browser/src/app/(main)/layout.tsx
@@ -14,7 +14,20 @@ export default function RootLayout({
         {children}
       </div>
       <div className="max-lg:hidden">
-        {children}
+        <div className="w-64 min-h-screen fixed border border-sky-800 z-50">
+          vvvv
+        </div>
+        <div className="flex">
+          <div className="flex-none min-h-screen w-64">
+          </div>
+          <div 
+            className="flex-auto border border-orange-800 pt-10"
+          >
+            <div className="border border-orange-800 max-w-[1024px] mx-auto">
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
