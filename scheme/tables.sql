6a91613bdca71115e73dcdafe357c1a6e451906a
xuerongNanopay <xuerong@nanopay.net>
2023-11-29 22:36:18 -0500

    update sql scheme

--- scheme/tables.sql
+++ scheme/tables.sql
@@ -1,5 +1,3 @@
-show databases;
-create database nbp;
 
 # login table
 create table login(
@@ -10,19 +8,24 @@ create table login(
     # field for email login
     email varchar(255) unique not null,
     password varchar(255) not null,
-    verifyCode varchar(16),
+    verifyCode varchar(8),
+#     verifyCodeAt timestamp null,
     isEmailLoginVerified boolean default false,
     status enum('active', 'disable', 'pending', 'delete') default 'pending',
 
     retrieveToken varchar(255) null,
-    retrieveTokenExpire timestamp null,
-    lastLogin timestamp,
+    retrieveTokenExpireAt timestamp null,
+    lastLoginAt timestamp null,
 #     loginAttempts
 #     lastLogin timestamp,
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
+alter table login change column retrieveTokenExpire retrieveTokenExpireAt timestamp null;
+alter table login change column lastLogin lastLoginAt timestamp null;
+alter table login change column createAt createdAt timestamp default current_timestamp;
+alter table login change column updateAt updatedAt timestamp default current_timestamp on update current_timestamp;
 
 # user one to one with login
 create table user(
@@ -35,24 +38,22 @@ create table user(
     dob date not null,
     address1 varchar(255),
     address2 varchar(255),
-    province varchar(8) references region(isoCode),
-    country varchar(4) references country(isoCode),
-    postCode varchar(64),
-    phoneNumber varchar(64),
-    pob varchar(4) references country(isoCode),
-    nationality varchar(4) references country(isoCode),
+    province varchar(4) references region(isoCode),
+    country varchar(4) references country(iso2Code),
+    postCode varchar(16),
+    phoneNumber varchar(32),
+    pob varchar(4) references country(iso2Code),
+    nationality varchar(4) references country(iso2Code),
 
-#     occupation: string,
-#     identityType: string,
-#     identityNumber: string
+    occupationId int references occupation(id),
 
     avatarUrl varchar(255),
 #     primaryAddress int, # reference to userAddress.
 #     primaryPhone int, # reference to userPhone
     status enum('active', 'suspend', 'delete') default 'active',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table account (
@@ -60,16 +61,16 @@ create table account (
     ownerId int not null references user(id),
 
     type enum('interac', 'bankAccount') not null,
-    bankId int not null references institution(id),
-    accountNo varchar(100),
-    branchNo varchar(100),
-    Iban varchar(100),
-    interacEmail varchar(255),
+#     bankId int not null references institution(id),
+#     accountNo varchar(100) null,
+#     branchNo varchar(100) null,
+#     Iban varchar(100) null,
+    interacEmail varchar(255) null,
     isDefault boolean default false,
 
-    status enum('active', 'pending', 'invalid', 'delete') default 'active',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    status enum('active', 'pending', 'disable', 'invalid', 'delete') default 'active',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 
@@ -82,18 +83,18 @@ create table contact(
     middleName varchar(255),
     lastName varchar(255) not null,
     dob date not null,
-    address1 varchar(255),
+    address1 varchar(255) not null,
     address2 varchar(255),
-    province varchar(8) references region(isoCode),
-    country varchar(4) references country(isoCode),
+    province varchar(8) not null references region(isoCode),
+    country varchar(4) not null references country(iso2Code),
     postCode varchar(64),
     phoneNumber varchar(64),
-    relationshipToOwner int not null references personal_relationship(id),
+    relationshipToOwner varchar(64) not null references personal_relationship(name),
 
     status enum('active', 'pending', 'invalid', 'delete') default 'pending',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table fee_detail(
@@ -105,19 +106,23 @@ create table fee_detail(
     amount integer not null,
     currency varchar(8) not null references currency(isoCode),
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 # SummaryTransaction only contain info that need for frontEnd
 create table summary_transaction(
     id serial primary key,
     ownerId int not null references user(id),
-    transactionId int references transaction(id),
+    transactionId int not null references transaction(id),
 
     errorMessage varchar(255) null,
-    sourceAccount int not null references account(id),
-    destinationAccount int not null references contact(id),
+    senderName varchar(255) null,
+    sourceAccountId int not null references account(id),
+    sourceAccountSummary varchar(255) null,
+    receiverName varchar(255) null,
+    destinationContactId int not null references contact(id),
+    destinationContactSummary varchar(255) null,
     sourceAmount INTEGER not null,
     destinationAmount INTEGER not null,
     sourceCurrency varchar(8) not null references currency(isoCode),
@@ -131,8 +136,8 @@ create table summary_transaction(
     # intial process success failed pending cancel.
     status enum('initial','waitingForPayment', 'process', 'complete', 'failed', 'refundInProgress', 'cancel') default 'initial',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 
 );
 
@@ -143,8 +148,8 @@ create table transaction(
     ownerId int not null references user(id),
 
     fromIp varchar(255) null,
-    sourceAccount int not null references account(id),
-    destinationAccount int not null references contact(id),
+    sourceAccountId int not null references account(id),
+    destinationContactId int not null references contact(id),
     sourceAmount INTEGER not null,
     destinationAmount INTEGER not null,
     sourceCurrency varchar(8) not null references currency(isoCode),
@@ -153,8 +158,8 @@ create table transaction(
     #     Initial Status for all transaction
     transferState int not null references transfer_state(id),
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 
@@ -168,8 +173,8 @@ create table transfer_state(
     retryAttempt int,
     transferStatus enum('initial', 'processing', 'pending', 'complete', 'rejected', 'retry', 'cancel') not null default 'initial',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table transferLog(
@@ -184,8 +189,8 @@ create table transferLog(
     subject varchar(255) not null,
     description TEXT,
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table quote(
@@ -201,95 +206,111 @@ create table quote(
 
     expired timestamp,
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table notification(
     id serial primary key,
     ownerId int not null references user(id),
-    level enum('info', 'warming', 'error') default 'info',
+    level enum('info', 'warming', 'error', 'debug') default 'info',
     subject varchar(250) null,
     content varchar(250) null,
     status enum('read', 'unread') default 'unread',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
+);
+
+create table identification(
+    id serial primary key,
+    ownerId int not null references user(id),
+    type varchar(32) not null,
+    num varchar(32) not null,
+
+    status enum('disable', 'active') default 'active',
+
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table currency(
     id serial primary key,
-    isoCode varchar(8) not null unique,
+    isoCode char(3) not null unique,
+    numCode char(3) not null,
     "decimal" smallint not null default 2,
-    name varchar(40) not null,
-    status enum('disable', 'enable') default 'enable',
+    name varchar(128) not null,
+    status enum('disable', 'active') default 'active',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 create index currency_isoCode_idx on currency(isoCode);
 
 create table country (
     id serial primary key,
-    isoCode varchar(4) not null unique,
-    name varchar(100) not null,
-    status enum('disable', 'enable') default 'enable',
+    iso2Code char(2) not null unique,
+    iso3Code char(3) not null unique,
+    numCode char(3) not null unique,
+    name varchar(128) not null,
+    status enum('disable', 'active') default 'active',
 
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
-create index country_isoCode_idx on country(isoCode);
+create index country_iso2Code_idx on country(iso2Code);
 
 create table region (
     id          serial primary key,
     isoCode     varchar(8)   not null unique,
-    countryCode varchar(4)   not null references country(isoCode),
-    abbreviation varchar(10) not null,
-    name        varchar(100) not null,
-    status enum('disable', 'enable') default 'enable',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    country char(2)   not null references country(iso2Code),
+    abbr        varchar(10) not null,
+    name        varchar(128) not null,
+    status enum('disable', 'active') default 'active',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 create index region_isoCode_idx on region(isoCode);
 
 create table institution (
     id serial primary key,
-    countryCode varchar(4)  not null references country(isoCode),
-    abbreviation varchar(10) null,
+    country varchar(4)  not null references country(iso2Code),
+    abbr varchar(10) null,
     name varchar(100) not null,
-    institutionNumber varchar(50),
-    status enum('disable', 'enable') default 'enable',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    institutionNum varchar(50),
+    status enum('disable', 'active') default 'active',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table occupation(
     id serial primary key,
-    name varchar(255) not null,
-    status enum('disable', 'enable') default 'enable',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    type varchar(64) not null,
+    description varchar(255) null,
+    status enum('disable', 'active') default 'active',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table api_log (
     id serial primary key,
     ownerId int not null references user(id),
-    transferId int references transfer_state(id),
+    transferId int null references transfer_state(id),
     provider varchar(255) not null,
-    name varchar(255) not null,
+    apiName varchar(255) not null,
     requestPayload TEXT null,
     responsePayload TEXT null,
-    httpStatus varchar(10) null,
+    httpResponseCode int null,
     errorMessage TEXT null,
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
 
 create table personal_relationship(
     id serial primary key,
-    name varchar(255) not null,
-    description varchar(255) not null,
-    status enum('disable', 'enable') default 'enable',
-    createAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp
+    type varchar(64) not null,
+    description varchar(255) null,
+    status enum('disable', 'active') default 'active',
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
 );
