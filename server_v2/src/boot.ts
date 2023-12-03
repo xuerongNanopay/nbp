be152d535f16ffd8757f8eaa23cc93f072470162
xuerongNanopay <xuerong@nanopay.net>
2023-12-03 01:14:27 -0500

    update with stupid idea

---
+++ server_v2/src/boot.ts
@@ -0,0 +1,5 @@
+async function boot() {
+  console.log("boot prerequisite")
+}
+
+export default boot
--- server_v2/src/index.ts
+++ server_v2/src/index.ts
@@ -1,18 +1,3 @@
-import http from 'http'
-// import { PrismaClient } from '@prisma/client'
-export const sever = http.createServer((_, resp) => {
-  resp.writeHead(200, {"Content-type": "application/json"})
-  resp.end(JSON.stringify({data: "It works"}))
-})
+import boot from './boot.js'
 
-// const _ = new PrismaClient()
-// client.login.create({})
-sever.listen(3000, () => {
-  console.log("Server running on 3000")
-})
-
-async function name() {
-  console.log('aa')
-}
-
-await name()
+boot()
