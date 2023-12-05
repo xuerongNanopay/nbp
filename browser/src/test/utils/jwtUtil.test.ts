35a5f24fb33cc29dca4d5c0d330336542b0caa86
xuerongNanopay <xuerong@nanopay.net>
2023-12-05 17:13:32 -0500

    unit test for encryption and decryption

--- browser/src/test/utils/jwtUtil.test.ts
+++ browser/src/test/utils/jwtUtil.test.ts
@@ -1,7 +1,9 @@
-import { encryptJWT } from "@/utils/jwtUtil"
+import { encryptJWT, decryptJWT } from "@/utils/jwtUtil"
 import type { JWT, JWTEncryptParams } from "@/utils/jwtUtil"
 
 test('JWT encryption and decryption', async () => {
+  const secret = 'My Secret'
+  const salt = 'My Salt'
   const payload: JWT = {
     loginId: 111,
     userId: 222
@@ -9,12 +11,20 @@ test('JWT encryption and decryption', async () => {
 
   const enParams: JWTEncryptParams<JWT> = {
     maxAge: 60 * 60, // 1 hour,
-    secret: "My Secret",
-    salt: "My Salt",
+    secret,
+    salt,
     token: payload
   }
 
   const encryptString = await encryptJWT<JWT>(enParams)
 
-  console.log(encryptString)
+  const msg = await decryptJWT({
+    secret,
+    salt,
+    token: encryptString
+  })
+
+  expect(msg).not.toBeNull();
+  expect(msg?.loginId).toEqual(payload.loginId)
+  expect(msg?.userId).toEqual(payload.userId)
 })
--- browser/src/utils/jwtUtil.ts
+++ browser/src/utils/jwtUtil.ts
@@ -12,18 +12,18 @@ import * as crypto from 'crypto'
 const now = () => (Date.now() / 1000) | 0
 const DEFAULT_MAX_AGE = 7 * 24 * 60 * 60 // default 7 days
 
-export async function encryptJWT<P extends JWTPayload = JWTPayload>(params: JWTEncryptParams<P>) {
+export async function encryptJWT<P extends JWTPayload = JWT>(params: JWTEncryptParams<P>) {
   const { token = {}, secret, maxAge = DEFAULT_MAX_AGE, salt } = params
-  const encryptKey = await generateEncryptionKey(secret, salt)
+  const key = await generateCryptoKey(secret, salt)
   return await new EncryptJWT(token)
     .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
     .setIssuedAt()
     .setExpirationTime(now() + maxAge)
     .setJti(crypto.randomUUID())
-    .encrypt(encryptKey)
+    .encrypt(key)
 }
 
-async function generateEncryptionKey(
+async function generateCryptoKey(
   secret: Parameters<typeof hkdf>[1],
   salt: Parameters<typeof hkdf>[2]
 ) {
@@ -36,8 +36,16 @@ async function generateEncryptionKey(
   )
 }
 
-export async function decrypt<Payload = JWT>() {
-
+export async function decryptJWT<P extends JWTPayload = JWT>(
+  params: JWTDecodeParams
+) : Promise<P | null>  {
+  const { token, secret, salt } = params
+  if ( !token ) return null;
+  const key = await generateCryptoKey(secret, salt)
+  const { payload } = await jwtDecrypt(token, key, {
+    clockTolerance: 15,
+  })
+  return payload as P
 }
 
 export async function sign() {
