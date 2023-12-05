e2d8c8f1b5fcb7fa6aad8a5ae49182ecaa2c5a64
xuerongNanopay <xuerong@nanopay.net>
2023-12-04 19:43:09 -0500

    api getway

--- lambda/python-thumbnail/handler.py
+++ lambda/python-thumbnail/handler.py
@@ -97,3 +97,63 @@ def s3_save_thumbnail_utl_to_dynamo(url_path, img_size):
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps(response)
     }
+
+def s3_get_item(event, context):
+    table = dynamodb.Table(dbtable)
+    response = table.get_item(Key={
+        'id': event['pathParameters']['id']
+    })
+    
+    item = response['Item']
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(item),
+        'isBase64Encoded': False
+    }
+
+def s3_delete_item(event, context):
+    item_id = event['pathParameters']['id']
+
+    response = {
+        "statusCode": 500,
+        "body": f"An error occured while deleting post {item_id}"
+    }
+
+    table = dynamodb.Table(dbtable)
+    response = table.delete_item(Key={
+        'id': item_id
+    })
+    all_good_response = {
+        "deleted": True,
+        "itemDeletedId": item_id
+    }
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(all_good_response),
+        }
+    return response
+
+def s3_get_thumbnail_urls(event, context):
+    table = dynamodb.Table(dbtable)
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(data)
+    }
