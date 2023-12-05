ea00639e6bdc24df0bb5af80643706e8636563e6
xuerongNanopay <xuerong@nanopay.net>
2023-12-05 11:07:22 -0500

    simple http server

--- server_v2/src/index.ts
+++ server_v2/src/index.ts
@@ -1,2 +1,17 @@
 // Run boot to initial dependency
 import './boot/index.js'
+
+import express from 'express'
+
+let counter = 0
+const PORT = process.env['HTTP_PORT'] || 3000
+const app = express()
+
+console.log("PORT: " + PORT)
+app.get("/", (_, res) => {
+  counter = counter + 1
+  console.log("count: ", counter)
+  res.json({message: 'count: ' + counter})
+});
+
+app.listen(PORT)
