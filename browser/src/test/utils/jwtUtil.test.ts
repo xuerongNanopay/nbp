9288274d971dc3906a8d5ddf2c08ad6465242606
xuerongNanopay <xuerong@nanopay.net>
2023-12-06 11:22:50 -0500

    add unit test

--- browser/src/test/utils/jwtUtil.test.ts
+++ browser/src/test/utils/jwtUtil.test.ts
@@ -1,30 +1,124 @@
 import { encryptJWT, decryptJWT } from "@/utils/jwtUtil"
 import type { JWT, JWTEncryptParams } from "@/utils/jwtUtil"
+import { JWEDecryptionFailed } from 'jose/errors'
 
-test('JWT encryption and decryption', async () => {
-  const secret = 'My Secret'
-  const salt = 'My Salt'
-  const payload: JWT = {
-    loginId: 111,
-    userId: 222
-  }
+describe('JWT encryption and decryption', () => {
 
-  const enParams: JWTEncryptParams<JWT> = {
-    maxAge: 60 * 60, // 1 hour,
-    secret,
-    salt,
-    token: payload
-  }
+  test('with no malice ', async () => {
+    const secret = 'My Secret'
+    const salt = 'My Salt'
+    const payload: JWT = {
+      loginId: 111,
+      userId: 222
+    }
+  
+    const enParams: JWTEncryptParams<JWT> = {
+      maxAge: 60 * 60, // 1 hour,
+      secret,
+      salt,
+      token: payload
+    }
+  
+    const encryptString = await encryptJWT<JWT>(enParams)
 
-  const encryptString = await encryptJWT<JWT>(enParams)
+    const msg = await decryptJWT({
+      secret,
+      salt,
+      token: encryptString
+    })
+  
+    expect(msg).not.toBeNull();
+    expect(msg?.loginId).toEqual(payload.loginId)
+    expect(msg?.userId).toEqual(payload.userId)
+  });
 
-  const msg = await decryptJWT({
-    secret,
-    salt,
-    token: encryptString
-  })
+  test('with malicious encrypted payload', async () => {
+    const secret = 'My Secret'
+    const salt = 'My Salt'
+    const payload: JWT = {
+      loginId: 111,
+      userId: 222
+    }
+  
+    const enParams: JWTEncryptParams<JWT> = {
+      maxAge: 60 * 60, // 1 hour,
+      secret,
+      salt,
+      token: payload
+    }
+  
+    const encryptString = await encryptJWT<JWT>(enParams)
 
-  expect(msg).not.toBeNull();
-  expect(msg?.loginId).toEqual(payload.loginId)
-  expect(msg?.userId).toEqual(payload.userId)
-})
+    try {
+      const msg = await decryptJWT({
+        secret,
+        salt,
+        token: encryptString + 'Bad Guy change the payload'
+      })
+      expect("Here").toBe("not reach")
+    } catch ( err ) {
+      expect(err).toBeInstanceOf(JWEDecryptionFailed)
+      expect((err as JWEDecryptionFailed).code).toBe('ERR_JWE_DECRYPTION_FAILED')
+    }
+  });
+
+  test('with malicious secret', async () => {
+    const secret = 'My Secret'
+    const salt = 'My Salt'
+    const payload: JWT = {
+      loginId: 111,
+      userId: 222
+    }
+  
+    const enParams: JWTEncryptParams<JWT> = {
+      maxAge: 60 * 60, // 1 hour,
+      secret,
+      salt,
+      token: payload
+    }
+  
+    const encryptString = await encryptJWT<JWT>(enParams)
+
+    try {
+      const msg = await decryptJWT({
+        secret: secret + "bad secret",
+        salt,
+        token: encryptString + 'Bad Guy change the payload'
+      })
+      expect("Here").toBe("not reach")
+    } catch ( err ) {
+      expect(err).toBeInstanceOf(JWEDecryptionFailed)
+      expect((err as JWEDecryptionFailed).code).toBe('ERR_JWE_DECRYPTION_FAILED')
+    }
+  });
+
+  test('with malicious salt', async () => {
+    const secret = 'My Secret'
+    const salt = 'My Salt'
+    const payload: JWT = {
+      loginId: 111,
+      userId: 222
+    }
+  
+    const enParams: JWTEncryptParams<JWT> = {
+      maxAge: 60 * 60, // 1 hour,
+      secret,
+      salt,
+      token: payload
+    }
+  
+    const encryptString = await encryptJWT<JWT>(enParams)
+
+    try {
+      const msg = await decryptJWT({
+        secret,
+        salt: salt + "bad sale",
+        token: encryptString + 'Bad Guy change the payload'
+      })
+      expect("Here").toBe("not reach")
+    } catch ( err ) {
+      expect(err).toBeInstanceOf(JWEDecryptionFailed)
+      expect((err as JWEDecryptionFailed).code).toBe('ERR_JWE_DECRYPTION_FAILED')
+    }
+  });
+});
