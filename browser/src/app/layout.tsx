75418f47081d72608f64f5866a94a2ecd57ea170
xuerongNanopay <xuerong@nanopay.net>
2023-12-08 01:29:28 -0500

    format code

--- browser/src/app/layout.tsx
+++ browser/src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NextUIProvider from '../providers/NextUIProvider'
-import { fetchSession } from '@/lib/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
