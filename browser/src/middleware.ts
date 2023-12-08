01c0ed7908e35b72c7d9ff3158ae335f6f380408
xuerongNanopay <xuerong@nanopay.net>
2023-12-07 21:01:14 -0500

    add todo

--- browser/src/middleware.ts
+++ browser/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// TODO: center auth handler.
+// TODO: IF cookie is about to expire and user still active then delay expiration for 1 hours.
 export async function middleware(request: NextRequest) {
   // console.log(request.url)
   // console.log(request.headers)
