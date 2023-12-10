338f22154303495bac6b51847e39a63c83fcb837
xuerongNanopay <xuerong@nanopay.net>
2023-12-10 16:23:42 -0500

    add institution

--- browser/src/schema/schema.sql
+++ browser/src/schema/schema.sql
@@ -126,3 +126,16 @@ create table region (
     updatedAt timestamp default current_timestamp on update current_timestamp
 );
 create index region_isoCode_idx on region(isoCode);
+
+create table institution (
+    id serial primary key,
+    status enum('disable', 'active') default 'active',
+    
+    country varchar(4)  not null references country(iso2Code),
+    abbr varchar(10) null,
+    name varchar(100) not null,
+    institutionNum varchar(50),
+
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
+);
