5298b13026f0051ea40a97d0b41b0af9f5c1ca8e
xuerongNanopay <xuerong@nanopay.net>
2023-12-10 16:16:08 -0500

    add Currency

--- browser/src/schema/schema.sql
+++ browser/src/schema/schema.sql
@@ -84,3 +84,17 @@ create table occupation(
     createdAt timestamp default current_timestamp,
     updatedAt timestamp default current_timestamp on update current_timestamp
 );
+
+create table currency(
+    id serial primary key,
+    status enum('disable', 'active') default 'active',
+
+    isoCode char(3) not null unique,
+    numCode char(3) not null,
+    "decimal" smallint not null default 2,
+    name varchar(128) not null,
+
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
+);
+create index currency_isoCode_idx on currency(isoCode);
