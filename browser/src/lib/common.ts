a8f8f3ccab0f83348ed006da7a4c53ea6da429a4
xuerongNanopay <xuerong@nanopay.net>
2023-12-11 17:37:57 -0500

    add return type for the common functions

--- browser/src/lib/common.ts
+++ browser/src/lib/common.ts
@@ -1,25 +1,31 @@
 import { getPrismaClient } from '@/utils/prisma'
+import { Country, Currency, Institution, Occupation, PersonalRelationship, Region } from '@prisma/client';
 
-export async function getRegionsByCountryCode(countryCode: string) {
-
+export async function getRegionsByCountryCode(
+  countryCode: string
+): Promise<Pick<Region, 'id' | 'abbr' | 'country' | 'name'> | null> {
+  return null
 }
 
-export async function getCountries() {
-
+export async function getCountries(): Promise<Pick<Country, 'id' | 'iso2Code' | 'name'> | null>{
+  return null
 }
 
-export async function getInstitutionsByCountryCode(countryCode: string) {
-
+export async function getInstitutionsByCountryCode(
+  countryCode: string
+) : Promise<Pick<Institution, 'id' | 'abbr' | 'country' | 'name'> | null>
+{
+  return null
 }
 
-export async function getPersinoalRelationships() {
-
+export async function getPersinoalRelationships(): Promise<Pick<PersonalRelationship, 'id' | 'type' | 'description'> | null> {
+  return null
 }
 
-export async function getOccupations() {
-
+export async function getOccupations(): Promise<Pick<Occupation, 'id' | 'type' | 'description'> | null> {
+  return null
 }
 
-export async function getCurrencies() {
-  
+export async function getCurrencies(): Promise<Pick<Currency, 'id' | 'isoCode' | 'decimal' | 'name'> | null> {
+  return  null
 }
