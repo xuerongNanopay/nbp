35144eae825733b008870bf5c643388fabbccb5e
xuerongNanopay <xuerong@nanopay.net>
2023-12-16 17:03:28 -0500

    bug fix

--- browser/src/app/nbp/(auth)/sign_out/page.tsx
+++ browser/src/app/nbp/(auth)/sign_out/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRef, useEffect } from "react";
 import { signOut } from "./actions";
+import { CircularProgress } from "@nextui-org/react";
 
 export default function SignOut() {
   const formRef = useRef<HTMLFormElement|null>(null);
@@ -9,6 +10,9 @@ export default function SignOut() {
   }, []);
 
   return (
-    <form ref={formRef} action={signOut}></form>
+    <div className="min-h-full grid place-content-center">
+      <form className="hidden" ref={formRef} action={signOut}></form>
+      <CircularProgress className="max-sm:mt-32" size="lg" color="primary" label="Loading..."></CircularProgress>
+    </div>
   )
 }
