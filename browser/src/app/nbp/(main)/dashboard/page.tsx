2380942c3d07f75f8fce9cd5d582d1dc793f584c
xuerongNanopay <xuerong@nanopay.net>
2023-12-19 18:52:57 -0500

    finish TODO on the dashboard

--- browser/src/app/nbp/(main)/dashboard/page.tsx
+++ browser/src/app/nbp/(main)/dashboard/page.tsx
@@ -3,12 +3,14 @@ import {
   RateCard,
   TransactionCard
 } from '@/components/dashboard'
+import { fetchSession } from '@/lib/session'
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await fetchSession()
   return (
     <>
       <h2 className="mx-2 text-2xl max-sm:text-xl font-semibold mb-4">
-        Good evening, {'TODO: Username'}
+        Welcome, {session?.user?.firstName}
       </h2>
       <div className="mx-2 flex max-sm:flex-col justify-evenly max-sm:items-center max-sm:justify-stretch">
         <RateCard className="w-full flex-auto sm:mr-2 max-sm:mb-4" rate={'TODO'}/>
