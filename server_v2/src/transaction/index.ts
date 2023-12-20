5b676d84cd15b3a4ae6068b41c59901360a13915
xuerongNanopay <xuerong@nanopay.net>
2023-12-19 21:43:22 -0500

    add axios

--- server_v2/src/transaction/index.ts
+++ server_v2/src/transaction/index.ts
@@ -1,8 +1,13 @@
 //entry point to start transaction process.
-function processTransaction(transaction) {
+
+const NBPTransferProcessor: any[] = []
+
+function processTransaction(transaction: any, processors: any) {
+  const processor = 1
   //TODO: find the transfer and process.
 }
 
 interface TransferProcessor<T> {
+  name: string
   process(transfer: T): void
 }
