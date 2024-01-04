5189e21c9d9f9375b832f6a5f899ae78583f1437
xuerongNanopay <xuerong@nanopay.net>
2024-01-04 02:41:44 -0500

    remove mobile transaction table

--- browser/src/app/nbp/(main)/transactions/page.tsx
+++ browser/src/app/nbp/(main)/transactions/page.tsx
@@ -1,31 +1,11 @@
-import { MobileTransactionTable, TransactionTable } from '@/components/table'
-import { NBPTransactionSummary } from '@/type'
+import { TransactionTable } from '@/components/table'
 
 export default async function Transactions() {
-
-  const transaction: NBPTransactionSummary= {
-    id: '1',
-    sendName: 'Xuerong Wu',
-    receiveName: 'vvvv tt',
-    created: new Date("2023-11-26"), // Or Date type
-    status: 'awaitPayent',
-    nbpReference: 'NP000000000000000',
-    sendAmount: "22.00 CAD",
-    receiveAmount: "4,520.34 PRK",
-    summary: "aaa -> vvv | avvv -> ccc"
-  }
-  
-  const testTransactions: NBPTransactionSummary[] = Array(44).fill(null).map((_, idx): NBPTransactionSummary => {
-    return {...transaction, id: idx.toString()}
-  })
-
-  const transactions = await Promise.resolve(testTransactions)
 
   return (
     <div>
       <h1 className="text-4xl font-bold mb-6 max-sm:text-2xl max-sm:mb-3">Transactions</h1>
-      <TransactionTable className="max-md:hidden" transactions={transactions}/>
-      <MobileTransactionTable className="md:hidden" transactions={transactions}/>
+      <TransactionTable/>
     </div>
   )
 }
--- browser/src/components/table/index.ts
+++ browser/src/components/table/index.ts
@@ -1,4 +1,3 @@
 export { default as TransactionTable } from './transactionTable'
 export { default as ContactTable } from './contactTable'
-export { default as MobileTransactionTable } from './mobileTransactionTable'
 export { default as MobileContactTable} from './mobileContactTable'
