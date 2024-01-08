c993eb0b85bace140245701f3dd52cb363004402
xuerongNanopay <xuerong@nanopay.net>
2024-01-08 18:18:14 -0500

    getToken

--- server_v2/src/partner/nbp/index.ts
+++ server_v2/src/partner/nbp/index.ts
@@ -1,12 +1,16 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
-import { CREDENTIAL, getAxios } from "./config.js";
-import { LOGGER } from "@/utils/logUtil.js";
+import axios, { AxiosError, AxiosResponse } from "axios"
+import { CREDENTIAL, getAxios } from "./config.js"
+import { LOGGER } from "@/utils/logUtil.js"
 
 import type {
   RawToken,
   Token
 } from './index.d.js'
-import { base64Encode } from "@/utils/bast64Util.js";
+import { Mutex } from "async-mutex"
+import { base64Encode } from "@/utils/bast64Util.js"
+
+let TOKEN :Token
+const mutex = new Mutex()
 
 async function hello(): Promise<String> {
   const endPoint = 'api/v2/Hello'
@@ -81,6 +85,28 @@ async function _requestToken(): Promise<Token|null> {
         JSON.stringify(err)
       )
     }
+  }
+  return null
+}
+
+async function _getToken(): Promise<Token | null> {
+  const maxRetryAttempts = 3
+  for (let i=0 ; i < maxRetryAttempts ; i++ ) {
+    if ( !TOKEN || new Date().getTime() + 30000 > TOKEN.Token_Expiry.getTime() ) {
+      const release = await mutex.acquire()
+      try {
+        if (!TOKEN || new Date().getTime() + 30000 > TOKEN.Token_Expiry.getTime() ) {
+          const newToken = await _requestToken()
+          if (!newToken) continue
+          TOKEN = newToken
+        }
+      } catch (err) {
+        LOGGER.error('nbp', 'function: _getToken', err)
+      } finally {
+        release()
+      }
+    }
+    return TOKEN
   }
   return null
 }
