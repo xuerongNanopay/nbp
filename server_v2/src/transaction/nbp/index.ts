7839ba4105cafb8a7b9c9380ef036df47137ca5a
xuerongNanopay <xuerong@nanopay.net>
2024-01-09 18:46:10 -0500

    add general interface for transaction process

--- server_v2/src/transaction/nbp/index.ts
+++ server_v2/src/transaction/nbp/index.ts
@@ -1,3 +1,25 @@
 //Processing transactions
 //Cancel transaction
 //Refund transaction
+
+// Initial CashIn.
+function initialCashIn(transactionId: number) {
+
+}
+
+// Once payment received processing the payment.
+function processTransaction(transactionId: number) {
+
+}
+
+function retryTransaction(transactionId: number, transferId: number) {
+
+}
+
+function cancelTransaction(transactionId: number) {
+
+}
+
+function refundTransaction(transactionId: number) {
+
+}
