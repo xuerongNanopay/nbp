5965d35649a2d1c24614d7ab27dd76bf2152b70d
xuerongNanopay <xuerong@nanopay.net>
2024-01-12 16:52:11 -0500

    communication setup

--- server_v2/src/hook/scotia_rtp_hook.ts
+++ server_v2/src/hook/scotia_rtp_hook.ts
@@ -5,20 +5,17 @@ import type { RTPHookRequest } from '@/partner/scotia_rtp/index.d.js';
 
 //TODO: get data from parameter.
 export async function scotiaRTPHookHandler(rawData: string) {
-  LOGGER.info('Hook: scotiaRTPHookHandler')
   const data = JSON.parse(rawData)
   //TODO: check if it is general or compact format.
   const encData = data['encData']
-
   //Using Our private to decrypt
   //Using our public to decript.
   const { plaintext } = await jose.generalDecrypt(encData, PRIVATE_KEY)
-
   const { payload } = await jose.compactVerify(plaintext, PEER_PUBLIC_KEY)
-  
   const requestPayload = new TextDecoder().decode(payload)
-
   const request: RTPHookRequest = JSON.parse(requestPayload)
 
+  LOGGER.info('Hook: scotiaRTPHookHandler', `payment_id: \`${request.payment_id}\``, `request: \`${requestPayload}\``)
 
+  
 }
