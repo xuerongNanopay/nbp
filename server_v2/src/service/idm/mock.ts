79765b5d314d71e91f7bfbf04753211e588e9eb9
xuerongNanopay <xuerong@nanopay.net>
2024-01-14 19:40:26 -0500

    fix ts type bug

--- server_v2/src/service/idm/mock.ts
+++ server_v2/src/service/idm/mock.ts
@@ -3,7 +3,7 @@ import { IDMService } from "./index.js";
 import { TransferResult } from "@/partner/idm/index.d.js";
 
 export const MOCKIDMService: IDMService = {
-  transferout: async (request: TransferReqeust): Promise<TransferResult> {
+  transferout: async (request: TransferReqeust): Promise<TransferResult> => {
     const isManualReview = Math.random() < 0.3;
     return {
       res: isManualReview ? 'MANUAL_REVIEW' : 'ACCEPT'
