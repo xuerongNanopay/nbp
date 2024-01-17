7e1a2e5a3dfdb83d3aa4925e52435685822308a4
xuerongNanopay <xuerong@nanopay.net>
2024-01-17 15:50:34 -0500

    add policy for cluster and node

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -35,3 +35,84 @@ module "nat_gateway" {
   private_subnet_az2_id = module.vpc.private_subnet_az2_id
   internet_gateway = module.vpc.internet_gateway
 }
+
+resource "aws_iam_role" "eks_cluster_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
+  tags = {
+    Name = "${var.app_name}-${var.environment}-eks-cluster-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name = var.app_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      module.vpc.private_subnet_az1_id,
+      module.vpc.private_subnet_az2_id,
+      module.vpc.public_subnet_az1_id,
+      module.vpc.public_subnet_az2_id
+    ]
+  }
+
+  depends_on = [ aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy ]
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  ]
+  
+  tags = {
+    Name = "${var.app_name}-${var.environment}-eks-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.eks_node_role.name
+}
