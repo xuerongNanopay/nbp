fe432cc445bc23b07e6acb9bc05eda300d5d0526
xuerongNanopay <xuerong@nanopay.net>
2024-01-16 21:58:38 -0500

    add public route table

--- terraform/vpn_module/main.tf
+++ terraform/vpn_module/main.tf
@@ -54,3 +54,36 @@ resource "aws_subnet" "public_subnet_za2" {
     Name = "${var.app_name}-${var.environment}-public-az2"
   }
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id     = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+    tags = {
+    Name = "${var.app_name}-${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_za1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_za2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_subnet" "private_subnet_za1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnet_az1_cidr
+  availability_zone = data.aws_availability_zones.available_zones.names[2]
+  
+  tags = {
+    Name = "${var.app_name}-${var.environment}-private-az1"
+  }
+}
--- terraform/vpn_module/variables.tf
+++ terraform/vpn_module/variables.tf
@@ -9,3 +9,4 @@ variable "app_name" {}
 variable "vpc_cidr"{}
 variable "public_subnet_az1_cidr" {}
 variable "public_subnet_az2_cidr" {}
+variable "private_subnet_az1_cidr" {}
