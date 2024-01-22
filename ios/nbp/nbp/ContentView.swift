c24532a71f11aab354af6b79d05d58f71b4098b7
xuerongNanopay <xuerong@nanopay.net>
2024-01-22 01:18:45 -0500

    add ios project

---
+++ ios/nbp/nbp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nbp
+//
+//  Created by Xuerong on 2024-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
