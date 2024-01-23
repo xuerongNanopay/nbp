b343b84a3423b3a8b5daa213b099ce6a9f8b1e9a
xuerongNanopay <xuerong@nanopay.net>
2024-01-22 22:26:39 -0500

    reset project

---
+++ ios/nbp/nbp/Views/Authentications/SignInView.swift
@@ -0,0 +1,99 @@
+//
+//  SignInView.swift
+//  nbp
+//
+//  Created by Xuerong on 2024-01-22.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        let _ = print("SignIn creating")
+        VStack {
+            Image("nbp_auth_header")
+                .resizable()
+                .frame(width: 400, height: 150)
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("Welcome Back")
+                    .font(.title)
+                    .bold()
+                    .padding(.bottom, 10)
+                VStack(alignment: .leading) {
+                    Text("Email or Username")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                        .padding(.bottom, -5)
+                    TextField("", text: $email)
+                        .padding(10)
+                        .accentColor(Color("green_700"))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("green_700"), lineWidth: 2)
+                        }
+                }
+                .padding(.bottom, 10)
+                
+                VStack(alignment: .leading) {
+                    Text("Password")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                        .padding(.bottom, -5)
+                    SecureField("", text: $password)
+                        .padding(10)
+                        .accentColor(Color("green_700"))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("green_700"), lineWidth: 2)
+                        }
+                }
+                //TODO: change to Nav Link
+                Text("ForgetPassword?")
+                    .foregroundStyle(.gray)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.top, 5)
+                
+                Button(action: {
+                    print("Sign In button click")
+                }) {
+                  Text("Sign In")
+                    .font(.headline)
+                    .bold()
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color("green_700"))
+                    .cornerRadius(10)
+                }
+                .padding(.top, 15)
+                
+                HStack() {
+                    Text("Not a User Yet?")
+                    
+                    Button(action: {
+                        print("Sign Up button click")
+                    }) {
+                        Text("Create an account")
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top)
+                
+                
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
--- ios/nbp/nbp/Views/ContentView.swift
+++ ios/nbp/nbp/Views/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    var hasSession = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if hasSession {
+            SignInView()
+        } else {
+            DashBoardView()
         }
-        .padding()
     }
 }
 
