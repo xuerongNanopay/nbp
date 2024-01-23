915e5799ffe944e049a78c3affef4ebbfaff9eec
xuerongNanopay <xuerong@nanopay.net>
2024-01-22 22:14:29 -0500

    add ios project

---
+++ ios/nbp/nbp/nbpApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbpApp.swift
+//  nbp
+//
+//  Created by Xuerong on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct nbpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
