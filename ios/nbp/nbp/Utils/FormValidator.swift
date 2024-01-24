1ca3ef07d922607f223f0f036a8ba19a9b9385b0
xuerongNanopay <xuerong@nanopay.net>
2024-01-24 00:31:57 -0500

    save change

--- ios/nbp/nbp/Utils/FormValidator.swift
+++ ios/nbp/nbp/Utils/FormValidator.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 struct FormValidator {
+    
+    static func trim(_ value: String) -> String {
+        return value.trimmingCharacters(in: .whitespaces)
+    }
+    
     static func required(_ value: String) -> String {
         return value.isEmpty ? "Required" : ""
     }
