eeff2fd9a45cb69af8ec062322fff55d885d46ad
xuerongNanopay <xuerong@nanopay.net>
2024-01-23 22:41:11 -0500

    Authentication router finish

--- ios/nbp/nbp/Views/Authentications/SignInView.swift
+++ ios/nbp/nbp/Views/Authentications/SignInView.swift
@@ -11,64 +11,75 @@ struct SignInView: View {
     @State var email: String = ""
     @State var password: String = ""
     
+    enum AuthRouter {
+        case SignUp, ForgetPassword
+    }
     var body: some View {
         let _ = print("SignIn creating")
-        VStack {
-            Image("nbp_auth_header")
-                .resizable()
-                .frame(width: 400, height: 150)
-            
-            Spacer()
-            
-            VStack(alignment: .leading) {
-                Text("Welcome Back")
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, 10)
+        NavigationStack {
+            VStack {
+                Image("nbp_auth_header")
+                    .resizable()
+                    .frame(width: 400, height: 150)
                 
-                ADInput(title: "Email or Username", value: $email).padding(.bottom, 10)
+                Spacer()
                 
-                ADInput(title: "Password", value: $password, adInputType: .password).padding(.bottom, 10)
-                
-                //TODO: change to Nav Link
-                Text("ForgetPassword?")
-                    .foregroundStyle(.gray)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .padding(.top, 5)
-                
-                Button(action: {
-                    print("Sign In with email: \(email) and password: \(password)")
-                }) {
-                  Text("Sign In")
-                    .font(.headline)
-                    .bold()
-                    .padding()
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .background(Color("green_700"))
-                    .cornerRadius(10)
-                }
-                .padding(.top, 15)
-                
-                HStack() {
-                    Text("Not a User Yet?")
+                VStack(alignment: .leading) {
+                    Text("Welcome Back")
+                        .font(.title)
+                        .bold()
+                        .padding(.bottom, 10)
+                    
+                    ADInput(title: "Email or Username", value: $email).padding(.bottom, 10)
+                    
+                    ADInput(title: "Password", value: $password, adInputType: .password).padding(.bottom, 5)
+                    
+                    NavigationLink(value: AuthRouter.SignUp) {
+                        Text("Forget Password?")
+                            .foregroundColor(.gray)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
                     
                     Button(action: {
-                        print("Sign Up button click")
+                        print("Sign In with email: \(email) and password: \(password)")
                     }) {
-                        Text("Create an account")
-                            .foregroundStyle(.green300)
+                    Text("Sign In")
+                        .font(.headline)
+                        .bold()
+                        .padding()
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("green_700"))
+                        .cornerRadius(10)
                     }
+                    .padding(.top, 15)
+                    
+                    HStack() {
+                        Text("Not a User Yet?")
+                        NavigationLink(value: AuthRouter.SignUp) {
+                            Text("Create an account").foregroundColor(.green300)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.top)
+                    
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.top)
+                .padding(.horizontal)
                 
-                
+                Spacer()
             }
-            .padding(.horizontal)
-            
-            Spacer()
+            .navigationDestination(for: AuthRouter.self) { route in
+                switch route {
+                case .SignUp:
+                    SignUpView()
+                case .ForgetPassword:
+                    Text("TODO: ForgetPassword")
+                }
+            }
+            .navigationTitle("Sign In")
+            .navigationBarHidden(true)
         }
+        .accentColor(.green700)
     }
 }
 
