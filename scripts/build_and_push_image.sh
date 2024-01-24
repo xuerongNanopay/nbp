122635e26ba00555cb8238f59106b408b1bc2ced
xuerongNanopay <xuerong@nanopay.net>
2024-01-24 12:39:33 -0500

    script for upload image

--- scripts/build_and_push_image.sh
+++ scripts/build_and_push_image.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+NBP_BROWER_CONTAINER_NAME=xuerong/nbp-browser
+NBP_SERVER_CONTAINER_NAME=xuerong/nbp-server
+
+docker build -t ${NBP_BROWER_CONTAINER_NAME} $SCRIPT_DIR/../browser --no-cache
+docker build -t ${NBP_SERVER_CONTAINER_NAME} $SCRIPT_DIR/../server_v2 --no-cache
+
+docker push $NBP_BROWER_CONTAINER_NAME
+docker push $NBP_SERVER_CONTAINER_NAME
