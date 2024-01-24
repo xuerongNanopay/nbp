e0e78af06b7421e4ee0fd46149e26491a6377880
xuerongNanopay <xuerong@nanopay.net>
2024-01-23 20:39:36 -0500

    add jenkin

--- ios/nbp/nbp/Views/Authentications/SignUpView.swift
+++ ios/nbp/nbp/Views/Authentications/SignUpView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var rePassword: String = ""
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
