da8a17afbd38a957adc07892837bbaf39d37ee36
xuerongNanopay <xuerong@nanopay.net>
2024-01-26 14:36:50 -0500

    add submit spinner to SignIn View

--- ios/nbp/nbp/Views/Authentications/SignInView.swift
+++ ios/nbp/nbp/Views/Authentications/SignInView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SignInView: View {
     @State var signInData = SignInFormData()
+    @State var isSubmitting = false
     
     enum AuthRouter {
         case SignUp, ForgetPassword
@@ -16,73 +17,89 @@ struct SignInView: View {
     var body: some View {
         let _ = print("SignIn creating")
         NavigationStack {
-            VStack {
-                Image("nbp_auth_header")
-                    .resizable()
-                    .frame(width: 400, height: 150)
-                
-                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("Welcome Back")
-                        .font(.title)
-                        .bold()
-                        .padding(.bottom, 10)
+            ZStack {
+                VStack {
+                    Image("nbp_auth_header")
+                        .resizable()
+                        .frame(width: 400, height: 150)
                     
-                    ADInput(title: "Email or Username", value: $signInData.email, hint: signInData.emailHint).padding(.bottom, 10)
+                    Spacer()
                     
-                    ADInput(title: "Password", value: $signInData.password, adInputType: .password, hint: signInData.passwordHint).padding(.bottom, 5)
-                    
-                    NavigationLink(value: AuthRouter.ForgetPassword) {
-                        Text("Forget Password?")
-                            .foregroundColor(.gray)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                    }
-                    
-                    Button(action: {
-                        if signInData.isValid() {
-                            print("Sign In form Success\(signInData)")
+                    VStack(alignment: .leading) {
+                        Text("Welcome Back")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 10)
+                        
+                        ADInput(title: "Email or Username", value: $signInData.email, hint: signInData.emailHint).padding(.bottom, 10)
+                        
+                        ADInput(title: "Password", value: $signInData.password, adInputType: .password, hint: signInData.passwordHint).padding(.bottom, 5)
+                        
+                        NavigationLink(value: AuthRouter.ForgetPassword) {
+                            Text("Forget Password?")
+                                .foregroundColor(.gray)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
                         }
-                    }) {
-                    Text("Sign In")
-                        .font(.headline)
-                        .bold()
-                        .padding()
-                        .foregroundColor(.white)
+                        
+                        Button(action: {
+                            if signInData.isValid() {
+                                print("Sign In form Success\(signInData)")
+                                isSubmitting = true
+                            }
+                        }) {
+                        Text("Sign In")
+                            .font(.headline)
+                            .bold()
+                            .padding()
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("green_700"))
+                            .cornerRadius(10)
+                        }
+                        .padding(.top, 15)
+                        
+                        HStack() {
+                            Text("Not a User Yet?")
+                            NavigationLink(value: AuthRouter.SignUp) {
+                                Text("Create an account").foregroundColor(.green300)
+                            }
+                        }
                         .frame(maxWidth: .infinity)
-                        .background(Color("green_700"))
-                        .cornerRadius(10)
+                        .padding(.top)
+                        
                     }
-                    .padding(.top, 15)
+                    .padding(.horizontal)
                     
-                    HStack() {
-                        Text("Not a User Yet?")
-                        NavigationLink(value: AuthRouter.SignUp) {
-                            Text("Create an account").foregroundColor(.green300)
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.top)
-                    
+                    Spacer()
+                    Spacer()
+                    Spacer()
                 }
-                .padding(.horizontal)
+                .navigationDestination(for: AuthRouter.self) { route in
+                    switch route {
+                    case .SignUp:
+                        SignUpView()
+                    case .ForgetPassword:
+                        ForgetPasswordView()
+                    }
+                }
+                .navigationTitle("Sign In")
+                .navigationBarHidden(true)
+                .onAppear {
+                    signInData.resetHint()
+                }
                 
-                Spacer()
-                Spacer()
-                Spacer()
-            }
-            .navigationDestination(for: AuthRouter.self) { route in
-                switch route {
-                case .SignUp:
-                    SignUpView()
-                case .ForgetPassword:
-                    ForgetPasswordView()
+                if isSubmitting {
+                    VStack {
+                        Spacer()
+                        ProgressView()
+                            .controlSize(.large)
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(Color.gray.opacity(0.5))
+                        Spacer()
+                    }
+                    .ignoresSafeArea()
                 }
-            }
-            .navigationTitle("Sign In")
-            .navigationBarHidden(true)
-            .onAppear {
-                signInData.resetHint()
             }
         }
         .accentColor(.green700)
