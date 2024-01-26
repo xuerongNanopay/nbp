08fcd1220438d1c63fe4f8501b52a9b8ae157fa6
xuerongNanopay <xuerong@nanopay.net>
2024-01-26 18:42:48 -0500

    finish authentication

--- ios/nbp/nbp/Views/Authentications/ForgetPasswordView.swift
+++ ios/nbp/nbp/Views/Authentications/ForgetPasswordView.swift
@@ -15,43 +15,80 @@ struct ForgetPasswordView: View {
     
     var body: some View {
         let _ = print("ForgetPassword View creating")
-        VStack {
-            Spacer()
-            Text("Forget Password?")
-                .font(.title)
-                .bold()
-                .padding(.bottom, 10)
-            Text("Enter the email you used to create your account in order to reset your password")
-                .font(.subheadline)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 20)
-            
-            ADInput(title: "Email", value: $forgetPasswordData.email).padding(.bottom, 10)
-            
-            Button(action: {
-//                print("Forget password: \(email)")
-            }) {
-            Text("Submit")
-                .font(.headline)
-                .bold()
-                .padding()
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .background(Color("green_700"))
-                .cornerRadius(10)
+        ZStack {
+            VStack {
+                Spacer()
+                Text("Forget Password?")
+                    .font(.title)
+                    .bold()
+                    .padding(.bottom, 10)
+                Text("Enter the email you used to create your account in order to reset your password")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                
+                ADInput(title: "Email", value: $forgetPasswordData.email, hint: forgetPasswordData.emailHint).padding(.bottom, 10)
+                
+                Button(action: {
+                    if forgetPasswordData.isValid() {
+                        Task {
+                            print("Forget passwrod form Success\(forgetPasswordData)")
+                            isSubmitting = true
+                            
+                            try await Task.sleep(nanoseconds: UInt64(4 * Double(NSEC_PER_SEC)))
+                            
+                            isSubmitting = false
+                            showAlert = true
+                        }
+                    } else {
+                        alertMessage = "Please check all your inputs."
+                        showAlert = true
+                    }
+                }) {
+                Text("Submit")
+                    .font(.headline)
+                    .bold()
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color("green_700"))
+                    .cornerRadius(10)
+                }
+                .padding(.top, 15)
+                .alert("Error", isPresented: $showAlert) {
+                    Button("Close") {
+                        alertMessage = ""
+                    }
+                } message: {
+                    Text(alertMessage)
+                }
+                
+                Spacer()
+                Spacer()
             }
-            Spacer()
-            Spacer()
-        }
-        .padding(.horizontal)
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                Image("nbp_navbar_icon")
-                    .resizable()
-                    .frame(width: 35, height: 35)
+            .padding(.horizontal)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Image("nbp_navbar_icon")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            
+            if isSubmitting {
+                VStack {
+                    Spacer()
+                    ProgressView()
+                        .controlSize(.large)
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.gray.opacity(0.5))
+                    Spacer()
+                }
+                .ignoresSafeArea()
             }
         }
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
